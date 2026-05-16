4912c613ad54fefc36bb637fad12e9856884195b
amberleyvs <amberley.vidya41@ui.ac.id>
2026-05-16 21:44:23 +0700

    Fix chat-async folder structure

---
+++ chat-async/src/bin/client.rs
@@ -0,0 +1,55 @@
+use futures_util::SinkExt;
+use futures_util::stream::StreamExt;
+use http::Uri;
+use tokio::io::{AsyncBufReadExt, BufReader};
+use tokio_websockets::{ClientBuilder, Message};
+
+#[tokio::main]
+async fn main() -> Result<(), tokio_websockets::Error> {
+    let (mut ws_stream, _) =
+        ClientBuilder::from_uri(Uri::from_static("ws://127.0.0.1:2000"))
+            .connect()
+            .await?;
+
+    println!("Amberley's Computer - Welcome to chat! Type a message");
+
+    let stdin = tokio::io::stdin();
+    let mut stdin = BufReader::new(stdin).lines();
+
+    loop {
+        tokio::select! {
+            line = stdin.next_line() => {
+                match line {
+                    Ok(Some(line)) => {
+                        ws_stream.send(Message::text(line)).await?;
+                    }
+                    Ok(None) => break,
+                    Err(e) => {
+                        println!("Input error: {e}");
+                        break;
+                    }
+                }
+            }
+
+            msg = ws_stream.next() => {
+                match msg {
+                    Some(Ok(msg)) => {
+                        if let Some(text) = msg.as_text() {
+                            println!("From server: {text}");
+                        }
+                    }
+                    Some(Err(e)) => {
+                        println!("WebSocket error: {e}");
+                        break;
+                    }
+                    None => {
+                        println!("Server disconnected");
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    Ok(())
+}
